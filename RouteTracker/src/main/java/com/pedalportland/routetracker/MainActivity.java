dc769ab09ac9dca1e6824d6107ec86745595219d
Minh Vu <vnm241191@gmail.com>
2014-03-08 21:16:21 -0800

    Adding checking GPS when users start tracking

--- RouteTracker/src/main/java/com/pedalportland/routetracker/MainActivity.java
+++ RouteTracker/src/main/java/com/pedalportland/routetracker/MainActivity.java
@@ -5,12 +5,14 @@ import com.pedalportland.routetracker.util.SystemUiHider;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -176,12 +178,6 @@ public class MainActivity extends Activity {
     @Override
     public void onResume() {
         try {
-            LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-            if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
-            {
-                Intent GPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(GPS);
-            }
             super.onResume(); // call the super method
         }
         catch (Exception ex){
@@ -358,7 +354,12 @@ public class MainActivity extends Activity {
                     if (isChecked) {
                         try {
                             // Start the route tracking
-                            routeTracker.startTracking();
+                            LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+                            if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                                Intent GPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(GPS);
+                            routeTracker.startTracking();}
+                            else{routeTracker.startTracking();}
                         }
                         catch(RouteTrackerException ex) {
                             ErrorDialog(R.string.rt_error_start);
